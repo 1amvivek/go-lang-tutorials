b5e3e664d985bcb25defd016f5c67618f6bc2a88
1amvivek <viveklakshmanan@live.com>
2017-10-14 22:25:34 -0700

    creating repo

---
+++ data-types.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	a := 24
+	b := "golang"
+	c := 0.65
+	d := true
+	e := "Hello, world!!!"
+	f := `This is vivek`
+	g := 'M'
+
+	fmt.Printf("%v \n", a)
+	fmt.Printf("%v \n", b)
+	fmt.Printf("%v \n", c)
+	fmt.Printf("%v \n", d)
+	fmt.Printf("%v \n", e)
+	fmt.Printf("%v \n", f)
+	fmt.Printf("%v \n", g)
+}
